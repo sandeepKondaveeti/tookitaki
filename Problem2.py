d53425367117b52f3686a3c4fab757378b010f24
sandeepKondaveeti <sandeep.sunny3026@gmail.com>
2019-02-12 18:00:13 +0530

    Update Problem2.py

--- Problem2.py
+++ Problem2.py
@@ -14,55 +14,70 @@ class Graph():
     def __init__(self, vertices):
         self.graph = defaultdict(list)
         self.V = vertices
+        for i in range(0,self.V):
+            self.graph[i].append(None)
         self.globalSum = 0
+        self.globalList = []
 
     def addEdge(self, u, v, w):
         self.graph[u].append((v,w))
 
     def isCyclicUtil(self, v, visited, recStack, sum):
 
-        # Mark current node as visited and
-        # adds to recursion stack
         visited[v] = True
         recStack[v] = True
 
-        # Recur for all neighbours
-        # if any neighbour is visited and in
-        # recStack then graph is cyclic
-        for neighbour in self.graph[v]:
-            if visited[neighbour[0]] == False:
-                if self.isCyclicUtil(neighbour[0], visited, recStack, sum+neighbour[1]) == True:
-                    if self.globalSum < sum+neighbour[1]:
-                        self.globalSum = sum++neighbour[1]
-                return True
-            elif recStack[neighbour[0]] == True:
-                if self.globalSum < sum+neighbour[1]:
-                    self.globalSum = sum+neighbour[1]
-                return True
 
-        # The node needs to be poped from
-        # recursion stack before function ends
-        recStack[v] = False
-        #TODO - remove
-        #visited[v] = False
+        for neighbour in self.graph[v]:
+            if neighbour is not None:
+                if visited[neighbour[0]] == False:
+                    if self.isCyclicUtil(neighbour[0], visited, recStack, sum+neighbour[1]) == True:
+                        if self.globalSum < sum+neighbour[1]:
+                            self.globalSum = sum+neighbour[1]
+                            self.addVertextoList(visited, self.globalList)
+                    return True
+                elif recStack[neighbour[0]] == True:
+                    if self.globalSum < sum+neighbour[1]:
+                        self.globalSum = sum+neighbour[1]
+                        self.addVertextoList(visited, self.globalList)
+                    return True
+
+            # The node needs to be poped from
+            # recursion stack before function ends
+            recStack[v] = False
+            #TODO - remove
+            #visited[v] = False
         return False
 
     # Returns true if graph is cyclic else false
     def isCyclic(self):
-        visited = [False] * self.V
-        recStack = [False] * self.V
+        visited = [False] * (self.V+1)
+        recStack = [False] * (self.V+1)
         res = False
         for node in range(self.V):
             #if visited[node] == False:
-            visited = [False] * self.V
+            visited = [False] * (self.V+1)
             if self.isCyclicUtil(node, visited, recStack,0 ) == True:
                 res = True
         return res
 
+    def addVertextoList(self, stack, list):
+        list.clear()
+        for i in range(self.V+1):
+            if stack[i] == True:
+                list.append(i)
+
 df = pd.read_csv("data_Problem2.csv", index_col=False)
 df.dropna(inplace=True)
-vertices = len(df['FROM_NODE'].unique())
+vertices = max(int(max(df['TO_NODE'])), int(max(df['FROM_NODE'])))
 g = Graph(vertices)
 
 for i in range (0, len(df['FROM_NODE'])):
-    g.addEdge(df['FROM_NODE'][i], df['TO_NODE'][i], df['VALUE'][i])
+    if df['FROM_NODE'][i] is not "nan" and df['FROM_NODE'][i] is not "nan" and df['FROM_NODE'][i] is not "nan":
+        g.addEdge(int(df['FROM_NODE'][i]), int(df['TO_NODE'][i]), df['VALUE'][i])
+
+if g.isCyclic() == 1:
+    print("Graph has a cycle with maximum sum: ", g.globalSum, " and the cycle ", g.globalList)
+else:
+    print("Graph has no cycle")
+ 
